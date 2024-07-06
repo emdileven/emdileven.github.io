e74aef28fe8272c9cc058df72560c1c2658a2a88
Rais Zainuri <35108804+raiszainuri@users.noreply.github.com>
2024-07-06 09:10:51 +0700

    biodata changes

--- script.js
+++ script.js
@@ -1,51 +1,43 @@
 const alumniData = [
     {
-        "name": "Zacky Muchlas Dharmawan",
-        "nickname": "Zacky",
-        "campus": "Universitas Indonesia",
-        "ig": "https://instagram.com/zacky_md",
-        "linkedin": "https://www.linkedin.com/in/zacky-muchlas-dharmawan/",
-        "cv": "https://drive.google.com/open?id=1ypeara4TSDXEvWVLlMRovN7lAtlxVxzC"
-    },
-    {
-        "name": "Nicholas Yang",
-        "nickname": "NY",
-        "campus": "Universitas Sumatera Utara",
-        "ig": "https://instagram.com/nicholasyangg",
-        "linkedin": "https://www.linkedin.com/in/nicholas-yang925?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        "cv": "https://drive.google.com/open?id=1b593pcPlDnKZCTF3clLqzqlYImM8Ssai"
-    },
-    {
-        "name": "Mohammad Izamul Fikri Fahmi",
-        "nickname": "Izam",
-        "campus": "Politeknik Negeri Malang",
-        "ig": "https://www.instagram.com/izamulfi/",
-        "linkedin": "https://www.linkedin.com/in/izamulfikri/",
-        "cv": "https://drive.google.com/open?id=11vJwiPa5Npo4yjK8_1PvX9o-QjvuCr0j"
+        "name": "Agung Riyadi",
+        "nickname": "Agung",
+        "campus": "Universitas Gunadarma",
+        "ig": "https://www.instagram.com/_agung_r_/",
+        "linkedin": "https://www.linkedin.com/in/agungr10/",
+        "photo": "https://lh3.googleusercontent.com/d/1wxfHgHTEKHgAXAzzseHnk5vqlnR_7Tky"
     },
     {
         "name": "Ajri Inda Robby",
         "nickname": "Ajri",
         "campus": "Institut Teknologi Nasional Bandung",
-        "ig": "https://www.instagram.com/jri_inr/",
+        "ig": "https://www.instagram.com/jri_inr",
         "linkedin": "https://www.linkedin.com/in/ajri-robby-24ab91221/",
-        "cv": "https://drive.google.com/open?id=1Y5DjTItwUDgiojBnMvGUTtXFKVMMLuNT"
+        "photo": "https://lh3.googleusercontent.com/d/1Y5DjTItwUDgiojBnMvGUTtXFKVMMLuNT"
     },
     {
-        "name": "Fina Valentina",
-        "nickname": "Fina",
-        "campus": "Universitas Multimedia Nusantara",
-        "ig": "https://www.instagram.com/finvalnnk/",
-        "linkedin": "https://www.linkedin.com/in/fina-valentina-387256231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-        "cv": "https://drive.google.com/open?id=1gtPhOgdUbnxQtv5kHZjGRb5W440sblK-"
+        "name": "Amura Maulidi Fachry",
+        "nickname": "Amura",
+        "campus": "Universitas Telkom",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
-        "name": "Agung Riyadi",
-        "nickname": "Agung",
-        "campus": "Gunadarma",
-        "ig": "https://www.instagram.com/_agung_r_/",
-        "linkedin": "https://www.linkedin.com/in/agungr10/",
-        "cv": "https://drive.google.com/open?id=1wxfHgHTEKHgAXAzzseHnk5vqlnR_7Tky"
+        "name": "Arkana Bilal Imani",
+        "nickname": "Arkana",
+        "campus": "Institut Teknologi Sepuluh Nopember",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Farhan Zainadin Zidan Ginting",
+        "nickname": "Farhan",
+        "campus": "Universitas Harapan Medan",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
         "name": "Fariz Aziz Kurniawan",
@@ -53,47 +45,159 @@ const alumniData = [
         "campus": "UIN Sunan Kalijaga Yogyakarta",
         "ig": "https://www.instagram.com/lafarizo/",
         "linkedin": "https://www.linkedin.com/in/lafarizo/",
-        "cv": "https://drive.google.com/open?id=12bw9wVwf20ihDelbIPZBNqEMCPMADWyS"
+        "photo": "https://lh3.googleusercontent.com/d/12bw9wVwf20ihDelbIPZBNqEMCPMADWyS"
+    },
+    {
+        "name": "Fina Valentina",
+        "nickname": "Fina",
+        "campus": "Universitas Multimedia Nusantara Jakarta",
+        "ig": "https://www.instagram.com/finvalnnk/",
+        "linkedin": "https://www.linkedin.com/in/fina-valentina-387256231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "photo": "https://lh3.googleusercontent.com/d/1gtPhOgdUbnxQtv5kHZjGRb5W440sblK-"
+    },
+    {
+        "name": "Fitto Martcellindo",
+        "nickname": "Fitto",
+        "campus": "Institut Teknologi Perusahaan Listrik Negara",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Julieta Arneysa Nixieshakira",
+        "nickname": "Julieta",
+        "campus": "Universitas Telkom Surabaya",
+        "ig": "https://www.instagram.com/nixiaaa_",
+        "linkedin": "https://www.linkedin.com/in/julieta-arneysa-nixieshakira-0997562a0/",
+        "photo": "https://lh3.googleusercontent.com/d/1FSTXka0-5bry3rXlAZui5YfRfuRJ_YZL"
+    },
+    {
+        "name": "Maitsa Adinda Prana Putri",
+        "nickname": "Maitsa",
+        "campus": "Universitas Brawijaya",
+        "ig": "https://www.instagram.com/maitsaadinda/",
+        "linkedin": "https://www.linkedin.com/in/maitsaadinda/",
+        "photo": "https://lh3.googleusercontent.com/d/16NkFcwHJDTTYx2evQjW6DRaem544fgS-"
+    },
+    {
+        "name": "Mohammad Izamul Fikri Fahmi",
+        "nickname": "Mohammad",
+        "campus": "Politeknik Negeri Malang",
+        "ig": "https://www.instagram.com/izamulfi/",
+        "linkedin": "https://www.linkedin.com/in/izamulfikri/",
+        "photo": "https://lh3.googleusercontent.com/d/11vJwiPa5Npo4yjK8_1PvX9o-QjvuCr0j"
+    },
+    {
+        "name": "Muhamad Naufal",
+        "nickname": "Muhamad",
+        "campus": "Universitas Indraprasta PGRI",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
         "name": "Muhammad Alif Ilham",
-        "nickname": "Alif",
+        "nickname": "Muhammad",
         "campus": "Universitas Indonesia",
         "ig": "https://www.instagram.com/alifilhmm/",
         "linkedin": "https://www.linkedin.com/in/muhammadalifilham?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
-        "cv": "https://drive.google.com/open?id=1aU1Klw8RjHpg93tAJAFLpKWp6KjwPx5J"
+        "photo": "https://lh3.googleusercontent.com/d/1aU1Klw8RjHpg93tAJAFLpKWp6KjwPx5J"
+    },
+    {
+        "name": "Muhammad Habibi Ramadhan",
+        "nickname": "Muhammad",
+        "campus": "Universitas Gunadarma",
+        "ig": "https://www.instagram.com/habibi.ramadhannn/",
+        "linkedin": "https://www.linkedin.com/in/habibiramadhan",
+        "photo": "https://lh3.googleusercontent.com/d/19gHtGvu3iX2CVf159ZS2Q2GEpFK7BS-p"
+    },
+    {
+        "name": "Muhammad Mizzy",
+        "nickname": "Muhammad",
+        "campus": "Politeknik Negeri Semarang",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Naufal Ammar Saputra",
+        "nickname": "Naufal",
+        "campus": "Institut Teknologi Sepuluh Nopember",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Nicholas Yang",
+        "nickname": "Nicholas",
+        "campus": "Universitas Sumatera Utara",
+        "ig": "instagram.com/nicholasyangg",
+        "linkedin": "https://www.linkedin.com/in/nicholas-yang925?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "photo": "https://lh3.googleusercontent.com/d/1b593pcPlDnKZCTF3clLqzqlYImM8Ssai"
+    },
+    {
+        "name": "Ratstsan Nur Akmal Adiwijaya",
+        "nickname": "Ratstsan",
+        "campus": "Universitas Diponegoro",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Refaldi Agustian",
+        "nickname": "Refaldi",
+        "campus": "Institut Teknologi Sepuluh Nopember",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
         "name": "Regi Kurniawan",
         "nickname": "Regi",
-        "campus": "Institut Teknologi PLN",
+        "campus": "Institut Teknologi Perusahaan Listrik Negara",
         "ig": "https://www.instagram.com/regi.kt30?igsh=MTBsOXZnNDU0N2Npeg==",
         "linkedin": "https://www.linkedin.com/in/regi-kurniawan-777435290/",
-        "cv": "https://drive.google.com/open?id=1yPLTlHI-pxpOaPqcI0m8nSfze1Br2Map"
+        "photo": "https://lh3.googleusercontent.com/d/1yPLTlHI-pxpOaPqcI0m8nSfze1Br2Map"
+    },
+    {
+        "name": "Ro'Uf Aufalin Al Ghoitsal",
+        "nickname": "Ro'Uf",
+        "campus": "Universitas Negeri Semarang",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
         "name": "Safitri",
-        "nickname": "Fitri",
+        "nickname": "#VALUE!",
         "campus": "Universitas Janabadra",
         "ig": "https://www.instagram.com/saafitrisa?igsh=em9yc3JjMWJudXk4",
         "linkedin": "https://www.linkedin.com/in/safitri-safitri0212/",
-        "cv": "https://drive.google.com/open?id=1TVxOD5xcFQVojaOtps99d5Jq0HuoKIwl"
+        "photo": "https://lh3.googleusercontent.com/d/1TVxOD5xcFQVojaOtps99d5Jq0HuoKIwl"
     },
     {
-        "name": "Muhammad Habibi Ramadhan",
-        "nickname": "Bibim",
-        "campus": "Gunadarma",
-        "ig": "https://www.instagram.com/habibi.ramadhannn/",
-        "linkedin": "https://www.linkedin.com/in/habibiramadhan",
-        "cv": "https://drive.google.com/open?id=19gHtGvu3iX2CVf159ZS2Q2GEpFK7BS-p"
+        "name": "The Manuel Eric Saputra",
+        "nickname": "The",
+        "campus": "Universitas Dian Nuswantoro",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
     },
     {
-        "name": "Maitsa Adinda Prana Putri",
-        "nickname": "Dinda",
-        "campus": "Universitas Brawijaya",
-        "ig": "https://www.instagram.com/maitsaadinda/",
-        "linkedin": "https://www.linkedin.com/in/maitsaadinda/",
-        "photo": "Maitsa Adinda Prana Putri A006D4KX3486.jpg"
+        "name": "Tu Bagus Rahardi",
+        "nickname": "Tu",
+        "campus": "Universitas Labuhanbatu",
+        "ig": null,
+        "linkedin": null,
+        "photo": null
+    },
+    {
+        "name": "Zacky Muchlas Dharmawan",
+        "nickname": "Zacky",
+        "campus": "Universitas Indonesia",
+        "ig": "instagram.com/zacky_md",
+        "linkedin": "https://www.linkedin.com/in/zacky-muchlas-dharmawan/",
+        "photo": "https://lh3.googleusercontent.com/d/1ypeara4TSDXEvWVLlMRovN7lAtlxVxzC"
     }
 ];
 
@@ -105,7 +209,8 @@ function createAlumniCard(alumni) {
     card.className = 'alumni-card';
 
     const img = document.createElement('img');
-    img.src = alumni.photo;
+    if (alumni.photo === null) {img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/340px-Default_pfp.svg.png";}
+    else {img.src = alumni.photo;}
     img.alt = `${alumni.name} photo`;
     
     const name = document.createElement('h2');
